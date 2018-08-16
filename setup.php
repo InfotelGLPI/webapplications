56ee305f85cb6603d84645d8cfded1e3403980a8
keha <kevin.hainry@infotel.com>
2018-08-16 17:58:58 +0200

    Update number version for release

--- setup.php
+++ setup.php
@@ -105,7 +105,7 @@ function plugin_init_webapplications() {
 function plugin_version_webapplications() {
 
    return ['name'           => _n('Web application', 'Web applications', 2, 'webapplications'),
-                'version'        => '2.5.0',
+                'version'        => '2.5.1',
                 'license'        => 'GPLv2+',
                 'oldname'        => 'appweb',
                 'author'         => "<a href='http://infotel.com/services/expertise-technique/glpi/'>Infotel</a>",
