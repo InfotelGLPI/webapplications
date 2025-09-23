13285ae412821d0a39082dfe67b048ca6f623999
Thetsmr <thetsmr@gmail.com>
2025-09-23 14:26:55 +0200

    Update Github Scripts

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Only strings with domain specified are extracted (use Xt args of keyword param to set number of args needed)
+# Recherche tous les fichiers PHP récursivement
+find . -name '*.php' > php_files.list
 
-xgettext *.php */*.php */*.php --copyright-holder='Webapplications Development Team' --package-name='GLPI - Webapplications plugin' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
-	--keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t \
-	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t
+# Extraction avec xgettext
+xgettext --files-from=php_files.list \
+  --copyright-holder='Webapplications Development Team' \
+  --package-name='Webapplications - Accounts plugin' \
+  -o locales/glpi.pot \
+  -L PHP \
+  --add-comments=TRANS \
+  --from-code=UTF-8 \
+  --force-po \
+  --keyword=_n:1,2,4t \
+  --keyword=__s:1,2t \
+  --keyword=__:1,2t \
+  --keyword=_e:1,2t \
+  --keyword=_x:1c,2,3t \
+  --keyword=_ex:1c,2,3t \
+  --keyword=_nx:1c,2,3,5t \
+  --keyword=_sx:1c,2,3t
+
+# Nettoyage
+rm php_files.list
 
 # Append locales from Twig templates
 SCRIPT_DIR=$(dirname $0)
