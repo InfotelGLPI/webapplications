0b0cab965f621d3b6628b42c74b0aee29594f193
Thetsmr <thetsmr@gmail.com>
2026-06-27 11:56:27 +0200

    Update LICENSE

--- setup.php
+++ setup.php
@@ -14,7 +14,7 @@
 
  webapplications is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
+ the Free Software Foundation; either version 3 of the License, or
  (at your option) any later version.
 
  webapplications is distributed in the hope that it will be useful,
@@ -145,7 +145,7 @@ function plugin_version_webapplications()
     return [
         'name' => __('Appliance dashboard', 'webapplications'),
         'version' => PLUGIN_WEBAPPLICATIONS_VERSION,
-        'license' => 'GPLv2+',
+        'license' => 'GPLv3+',
         'oldname' => 'appweb',
         'author' => "<a href='https//blogglpi.infotel.com'>Infotel</a>, Xavier CAILLAUD",
         'homepage' => 'https://github.com/InfotelGLPI/webapplications',
